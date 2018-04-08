72f2914cb173b67b289c8650fa49e362f480266c
alisonstardust <zakulisami@o2.pl>
2018-04-08 18:03:43 +0200

    moving to element function

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-/*#C94277 #A27E6F #94524A*/
+const aboutUsLink = document.querySelector('.website_header--listAboutUs');
+const serviceLink = document.querySelector('.website_header--listService');
+const reviewsLink = document.querySelector('.website_header--listReviews');
+const contactLink = document.querySelector('.website_header--listContact');
+const about = document.querySelector('.website_about');
+const service = document.querySelector('.website_service');
+const reviews = document.querySelector('.website_reviews');
+const contact = document.querySelector('.website_contact');
+
+//moving to element events
+const moveToAbout = () => moveToElement(about);
+const moveToService = () => moveToElement(service);
+const moveToReviews = () => moveToElement(reviews);
+const moveToContact = () => moveToElement(contact);
+
+aboutUsLink.addEventListener("click", moveToAbout);
+serviceLink.addEventListener("click", moveToService);
+reviewsLink.addEventListener("click", moveToReviews);
+contactLink.addEventListener("click", moveToContact);
+
+//function moving to element itself
+function moveToElement(where) {
+  console.log(where)
+  where.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
